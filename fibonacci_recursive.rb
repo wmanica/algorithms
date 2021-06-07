46d41ea9500c7890a541ee4d6d3e169913e63555
walter <wmartins@deloitte.pt>
2021-06-07 08:16:35 +0100

    Fibonacci solution iterative

---
+++ fibonacci_recursive.rb
@@ -0,0 +1,24 @@
+# Recursive
+# We want to know what number will be in the fibonacci sequence position excluding the 0
+
+# These will have a problem of exponentially O(2^n)
+
+def fibonacci(n)
+    return n if (0..1).include? n
+    (fibonacci(n-1) + fibonacci(n-2))
+end
+
+f = ->(x) { x < 2 ? x : f[x-1] + f[x-2] }
+# p fibonacci(10)
+# p f[11]
+
+
+# Solution will be to use Memoization
+
+@cache = [0,1]
+def fib_memoization(n)
+  return @cache[n] if @cache[n]
+
+  @cache[n] = fib_memoization(n-1) + fib_memoization(n-2)
+end
+# p fib_memoization(50)
